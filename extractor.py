9c2bf235a91dcd621e8b62537f2a7f073ddedf3b
KTaskn <korai.kinjo@gmail.com>
2021-12-26 18:10:37 +0900

    コメントを追加

--- extractor.py
+++ extractor.py
@@ -10,6 +10,11 @@ import sys
 import argparse
 
 PRETRAINED_PATH = "./rgb_i3d_pretrained.pt"
+N_BATCHES = 5
+N_WORKERS = 5
+
+# Number of frames per feature
+F = 16
 
 class MyNet(nn.Module):
     def __init__(self):
@@ -20,10 +25,6 @@ class MyNet(nn.Module):
     def forward(self, video):
         video = video.transpose_(1, 2)
         return self.i3d(video).squeeze(2)
-
-N_BATCHES = 5
-N_WORKERS = 5
-CUDA = True
     
 def extract(dataset, net, n_batches=N_BATCHES, n_workers=N_WORKERS, cuda=False):
     net = net.cuda() if cuda else net
@@ -43,17 +44,19 @@ def extract(dataset, net, n_batches=N_BATCHES, n_workers=N_WORKERS, cuda=False):
             pbar.update(1)
     return torch.cat(outputs)
 
-F = 16
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("pathlist", help="Path to the dataset", type=str)
     parser.add_argument("output_path", help="Path to the output file", type=str)
+    parser.add_argument("--gpu", help="True if you want to use GPU", type=bool, default=True)
     
     args = parser.parse_args()
+    print(f"pathlist: {args.pathlist}")
+    print(f"output_path: {args.output_path}")
+    print(f"gpu: {args.gpu}")
     
-    path = args.pathlist
-    
-    with open(path) as f:
+    # Get the image path and label from a input file
+    with open(args.pathlist) as f:
         path_and_label = [
             row.split(" ")
             for row in f.read().split("\n")
@@ -63,17 +66,18 @@ if __name__ == "__main__":
         labels = [int(label) for _, label in path_and_label]
     
     ds = DataSet(paths_image, F=F)
+
+    # You can change the model here
     net = MyNet()
     with torch.no_grad():
-        outputs = extract(ds, net, cuda=CUDA)
+        outputs = extract(ds, net, cuda=args.cuda)
         
     ds_labels = DataSet(labels, F=F, func_extract=lambda X: 1 if sum(X) > 0 else 0)
     labels = torch.tensor([
         ds_labels.__getitem__(idx)
         for idx in range(ds_labels.__len__())
     ])
-    
-    
+        
     print(f"features_size: {outputs.size()}")
     print(f"labels_size: {labels.size()}")
     
