9d351612feaac42414a2156b36696f6b9d33f03b
KTaskn <korai.kinjo@gmail.com>
2022-01-05 17:28:15 +0900

    修正

--- extractors/i3d/extractor.py
+++ extractors/i3d/extractor.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from i3d import InceptionI3d
 from dataset import DataSet
 import argparse
+import pandas as pd
 
 PRETRAINED_PATH = "./rgb_i3d_pretrained.pt"
 N_BATCHES = 100
@@ -24,21 +25,24 @@ class MyNet(nn.Module):
     
 def extract(dataset, net, n_batches=N_BATCHES, n_workers=N_WORKERS, cuda=False):
     net = net.cuda() if cuda else net
+    net.eval()
     loader = torch.utils.data.DataLoader(
         dataset,
         batch_size=n_batches,
         shuffle=False,
         num_workers=n_workers)
 
-    outputs = []
-    with tqdm(total=len(loader), unit="batch") as pbar:
-        for videos in loader:
-            videos = videos.cuda() if cuda else videos
-            predict = net(videos)
-            predict = predict.cpu() if cuda else predict
-            outputs.append(predict)
-            pbar.update(1)
-    return torch.cat(outputs)
+    outputs, labels = [], []
+    with torch.no_grad():
+        with tqdm(total=len(loader), unit="batch") as pbar:
+            for videos, b_labels in loader:
+                videos = videos.cuda() if cuda else videos
+                predict = net(videos)
+                predict = predict.cpu() if cuda else predict
+                outputs.append(predict)
+                labels.append(b_labels)
+                pbar.update(1)
+    return torch.cat(outputs), torch.cat(labels)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -53,28 +57,33 @@ if __name__ == "__main__":
     
     # Get the image path and label from a input file
     with open(args.pathlist) as f:
-        path_and_label = [
+        grp_path_and_label = [
             row.split(" ")
             for row in f.read().split("\n")
             if row
         ]
-        paths_image = [path for path, _ in path_and_label]
-        labels = [int(label) for _, label in path_and_label]
+        df = pd.DataFrame({
+            "grp": [int(grp) for grp, _, _ in grp_path_and_label],
+            "path": [path for _, path, _ in grp_path_and_label],
+            "label": [int(label) for _, _, label in grp_path_and_label],
+        })
     
-    ds = DataSet(paths_image, F=F)
-
     # You can change the model here
     net = MyNet()
-    with torch.no_grad():
-        outputs = extract(ds, net, cuda=args.gpu)
-        
-    ds_labels = DataSet(labels, F=F, func_extract=lambda X: 1 if sum(X) > 0 else 0)
-    labels = torch.tensor([
-        ds_labels.__getitem__(idx)
-        for idx in range(ds_labels.__len__())
-    ])
+    outputs, labels = [], []
+    for grp, df_grp in df.groupby("grp"):
+        ds = DataSet(
+            df_grp["path"].tolist(),
+            df_grp["label"].tolist())
+
+        o, l = extract(ds, net, cuda=args.gpu)
     
-    outputs = outputs.unsqueeze(1)
+        o = o.unsqueeze(1)
+        outputs.append(o)
+        labels.append(l)
+                
+    outputs = torch.cat(outputs)
+    labels = torch.cat(labels)
         
     print(f"features_size: {outputs.size()}")
     print(f"labels_size: {labels.size()}")
