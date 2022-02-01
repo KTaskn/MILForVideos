5e9260ba77c9fb251f587710fa798354e845d009
KTaskn <korai.kinjo@gmail.com>
2022-02-01 18:12:18 +0900

    videoのインタフェースを追加

---
+++ tests/test_video.py
@@ -0,0 +1,104 @@
+from video import Extractor, VideoFeature
+import torch
+import pytest
+
+class TestVideoFeature:
+    def test_init(self):
+        path_list = []
+        labels = []
+        features = torch.tensor([])
+        video_feature = VideoFeature(path_list, labels, features)
+        assert video_feature.path_list == path_list
+        assert type(video_feature.labels) is torch.Tensor
+        assert video_feature.labels.tolist() == labels
+        assert type(video_feature.features) is torch.Tensor
+        assert all(video_feature.features == features)
+        
+        path_list = [1, 2, 3]
+        labels = [1, 2, 3]
+        features = torch.rand([3, 5])
+        VideoFeature(path_list, labels, features)
+    
+    
+    def test_pathlistとfeaturesは同じ長さの必要がある(self):
+        path_list = [1]
+        features = torch.tensor([])
+        with pytest.raises(Exception):
+            VideoFeature(path_list, features)
+            
+        path_list = [1, 2, 3]
+        features = torch.rand([5, 5])
+        with pytest.raises(Exception):
+            VideoFeature(path_list, features)
+        
+    
+    
+    def test_pathlistとlabelsは同じ長さの必要がある(self):
+        path_list = [1]
+        labels = [1, 2]
+        features = torch.tensor([1])
+        with pytest.raises(Exception):
+            VideoFeature(path_list, features)
+            
+        path_list = [1, 2, 3]
+        labels = [2, 3]
+        features = torch.rand([3, 5])
+        with pytest.raises(Exception):
+            VideoFeature(path_list, features)
+
+class TmpModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def __call__(self, x):
+        return x + 1
+       
+
+class TestExtractor:
+    def test_extractor(self):
+        path_list = ["1.jpg", "2.jpg", "3.jpg"]
+        labels = [1, 2, 3]
+        model = TmpModel()
+        
+        def parser(path):
+            if path == "1.jpg":
+                return torch.tensor([1])
+            elif path == "2.jpg":
+                return torch.tensor([2])
+            else:                
+                return torch.tensor([3])                
+        
+        extractor = Extractor(path_list, labels, model, parser)
+        video_feature = extractor.extract()
+        assert type(video_feature) is VideoFeature
+        assert video_feature.path_list == path_list
+        assert video_feature.labels.tolist() == labels
+        assert video_feature.features.size() == torch.Size([3, 1])
+        assert all(video_feature.features == torch.tensor([[2], [3], [4]]))
+        
+        
+        extractor = Extractor(path_list, labels, model, parser, n_batches=10, n_workers=10)
+        assert extractor.n_batches == 10
+        assert extractor.n_workers == 10
+        
+    
+    def test_extractor_2(self):
+        path_list = ["1.jpg", "2.jpg", "3.jpg"]
+        labels = [1, 2, 3]
+        model = TmpModel()
+        
+        def parser(path):
+            if path == "1.jpg":
+                return torch.tensor([1, 1])
+            elif path == "2.jpg":
+                return torch.tensor([2, 2])
+            else:                
+                return torch.tensor([3, 3])                
+        
+        extractor = Extractor(path_list, labels, model, parser)
+        video_feature = extractor.extract()
+        assert type(video_feature) is VideoFeature
+        assert video_feature.path_list == path_list
+        assert video_feature.labels.tolist() == labels
+        assert video_feature.features.size() == torch.Size([3, 2])
+        assert (video_feature.features == torch.tensor([[2, 2], [3, 3], [4, 4]])).all()
---
+++ video.py
@@ -0,0 +1,50 @@
+import torch
+from typing import List, Callable
+
+class VideoFeature:
+    def __init__(self, path_list: List[str], labels: List[int], features: torch.Tensor):
+        if len(path_list) != len(labels):
+            raise Exception(f"path_list and labels must be same length, but {len(path_list)} and {len(labels)}")
+        
+        if len(path_list) != features.size(0):
+            raise Exception(f"path_list and features must be same length, but {len(path_list)} and {features.size(0)}")
+        
+        self.path_list = path_list
+        self.features = features
+        self.labels = torch.tensor(labels)
+
+
+class _DataSet(torch.utils.data.Dataset):
+    def __init__(self, path_list: List[str], parser: Callable[[str], torch.Tensor]):
+        self.path_list = path_list
+        self.parser = parser
+        
+    def __len__(self):
+        return len(self.path_list)
+
+    def __getitem__(self, idx):
+        return self.parser(self.path_list[idx])
+
+class Extractor:
+    def __init__(self, path_list: List[str], labels: List[int], model: torch.nn.Module, parser: Callable[[str], torch.Tensor],
+                 n_batches=5, n_workers=5):
+        self.path_list = path_list
+        self.labels = labels
+        self.model = model
+        self.parser = parser
+        self.n_batches = n_batches
+        self.n_workers = n_workers
+    
+    def extract(self):
+        dataset = _DataSet(self.path_list, self.parser)        
+        loader = torch.utils.data.DataLoader(
+            dataset,
+            shuffle=False,
+            batch_size=self.n_batches,
+            num_workers=self.n_workers)
+        Y = torch.cat([
+            self.model(batch)
+            for batch in loader
+        ])
+        return VideoFeature(self.path_list, self.labels, Y)
+    
